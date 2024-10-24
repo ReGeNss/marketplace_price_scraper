93c2a2b932be2a4f2127ab700eade8c976811db5
REGENS_LAPTOP\regen <regens.global@gmail.com>
2024-10-25 00:20:31 +0300

    work onder brand parser, realized dublicate deleater, other

--- src/data_service/brand_parser.ts
+++ src/data_service/brand_parser.ts
@@ -1,73 +1,4 @@
-// const brandParser = (products: Product[]) => {
-//     let brands: Brand[] = [];
-//     for(let product of products){
-//         let brandName = "";
-//         let splitedTitle = product.title.split(' ');
-//         let productIndex = products.indexOf(product);
-//         products.splice(productIndex, 1);
-//         for(let word of splitedTitle ){
-//             if (word.includes("мл") ||
-//                 word.includes("л") ||
-//                 word.includes("500") ||
-//                 word.includes("250") ||
-//                 word.includes("0.5") ||
-//                 word.includes("0,5") ||
-//                 word.includes("зі") ||
-//                 word.includes("смаком") ||
-//                 word.includes("1л")
-//             ) continue;
-//             let counter = 0;
-//             let brandProducts: Product[] = [];
-//             for(let e of products){
-//                 if(e.title.includes(word)){
-//                     brandProducts.push(e);
-//                     counter++;
-//                 }
-//                 if(e.title.includes("воля")){
-//                     console.log("found");
-//                 }
-//                 if(counter >= 3){
-//                     brandName = brandName+' '+word;
-//                     brands.push({name: brandName.trim(), products: brandProducts});
-//                     break;
-//                 }
-//             }
-//         }
-//     }
-//     // console.log(brands);
-//     brands = specificateBrands(brands);
-//     // console.log(brands);
-//
-// }
-
-// const specificateBrands = (brands: Brand[]) => {
-//     let specificatedBrands: Brand[] = [];
-//     while(brands.length > 1) {
-//         for (let brand of brands) {
-//             let brandName = brand.name.toLowerCase();
-//             let brandIndex = brands.indexOf(brand);
-//             let splitedBrandName = brandName.toLowerCase().trim().split(' ');
-//             // console.log(splitedBrandName[0]);
-//             brands.splice(brandIndex, 1);
-//             for (let e of brands) {
-//                 let splitedName = e.name.toLowerCase().split(' ');
-//                 // console.log(splitedName[0].includes(splitedBrandName[0]))
-//                 // console.log(splitedName[0]);
-//                 // console.log(splitedBrandName[0]);
-//                 if (splitedName[0].includes(splitedBrandName[0])) {
-//                     // console.log(brands);
-//                     brand.products = brand.products.concat(e.products);
-//                     brands.splice(brands.indexOf(e), 1);
-//                 }
-//             }
-//             specificatedBrands.push(brand);
-//         }
-//     }
-//     console.log(specificatedBrands);
-//     return specificatedBrands;
-// }
-
-    const brandParser = (products: Product[]) => {
+ const brandParser = (products: Product[]) => {
         let brands: Brand[] = [];
         console.log('brandParser' + products.length);
         let otherProducts: Product[] = [];
@@ -76,16 +7,13 @@
             let splitedTitle = product.title.split(' ');
             // console.log('splitedTitle'+ splitedTitle);
             let productIndex = products.indexOf(product);
-            products.splice(productIndex, 1);
+            // products.splice(productIndex, 1);
             let brandName = "";
             for(let word of splitedTitle ){
                 let brandsProducts: Product[] = [];
                 if (word.includes("зі") ||
                     word.includes("смаком")
                 ) continue;
-
-
-
                 products.forEach(e=> {
                     // console.log(word.toLowerCase().trim());
                     // console.log(word.toLowerCase() === e.title.toLowerCase().split(' ')[0])
@@ -100,13 +28,13 @@
                     //     console.log('brandName ERR '+ brandName);
                     //     break;
                     // }
-                    brandsProducts.push(product);
+                    // brandsProducts.push(product);
                     console.log("Products now " +products.length);
                     console.log("brand now: "+brandsProducts.length);
                     brands.push({name: brandName.trim(), products: brandsProducts});
                     break;
                 }else{
-                    otherProducts.push(...brandsProducts);
+                    otherProducts.push(product);
                 }
 
             }
@@ -115,29 +43,73 @@
         }
         if(products.length > 0){
             // otherProductSpecificate(products, brands);
-            brands.push({name: "other", products: [...products]});
+            let otherProductFiltered = otherProductDuplicateRemove([...products,...otherProducts]);
+            brands.push({name: "other", products: otherProductFiltered});
         }
-        console.log(brands);
-        specificateBrands(brands);
-    }
+        let specificatedBrands = brandsDublicateDelete(brands);
+        let formatedBrands = otherProductSpecificate(specificatedBrands);
+        return extendBrandsName(formatedBrands);
+}
 
-const otherProductSpecificate = (products: Product[], brands: Brand[]) => {
-    console.log('otherProductSpecificate' + products.length);
+const extendBrandsName = (brands: Brand[]) => {
+    let indexOfOther = brands.findIndex(e => e.name.toLowerCase().includes('other'));
+    let otherProducts = brands.splice(indexOfOther, 1);
+    for(let brand of brands){
+        let products = brand.products;
+        let extendetName = "";
+        let splitedProductName = brand.name.split(' ');
+        for(let part of splitedProductName){
+            console.log(products.some(e => e.title.toLowerCase().includes(part.toLowerCase().trim() )));
+            if(products.some(e => e.title.toLowerCase().includes(part.toLowerCase().trim()))){
+                extendetName = extendetName + ' ' + part;
+            }
+        }
+        brand.name = extendetName.trim();
+    }
+    brands.push(...otherProducts);
+    console.log('extendBrandsName ');
+    console.log(brands);
+    return brands;
+}
+const otherProductDuplicateRemove = (products: Product[]) => {
+    console.log('otherProductDuplicate' + products.length);
+    let fitleredProducts: Product[] = [];
     for(let product of products){
+        const isDuplicate = fitleredProducts.some(oldProduct =>
+            oldProduct.marketplace === product.marketplace &&
+            oldProduct.title === product.title &&
+            oldProduct.currentPrice === product.currentPrice &&
+            oldProduct.oldPrice === product.oldPrice &&
+            oldProduct.imgSrc === product.imgSrc &&
+            oldProduct.volume === product.volume
+        );
+        if (!isDuplicate) {
+            fitleredProducts.push(product);
+        }
+    }
+    console.log('otherProductDuplicate');
+    console.log(fitleredProducts);
+    return fitleredProducts;
+}
+const otherProductSpecificate = (brands: Brand[]) => {
+    let otherProducts = brands.find(e => e.name === "other")?.products;
+    if(otherProducts == undefined) return brands;
+    for(let product of otherProducts){
         let splitedTitle = product.title.toLowerCase().split(' ');
         for(let brand of brands){
             if(brand.name.toLowerCase().includes(splitedTitle[0])){
+                otherProducts.splice(otherProducts.indexOf(product), 1);
                 brand.products.push(product);
                 console.log('OTHER' + brand.name);
-
             }
         }
     }
-    // console.log('otherProductSpecificateFINAL' + products.length);
+    console.log('otherProductSpecificate!!!!!!!!');
+    console.log(brands);
     return brands;
 }
 
-const specificateBrands = (brands: Brand[]) => {
+const brandsDublicateDelete = (brands: Brand[]) => {
     let specificatedBrands: Brand[] = [];
     let count = 0;
     brands.forEach(e => {
@@ -169,6 +141,7 @@ const specificateBrands = (brands: Brand[]) => {
         count2 += e.products.length;
     })
     console.log(count2);
+    return specificatedBrands;
 }
 
 
